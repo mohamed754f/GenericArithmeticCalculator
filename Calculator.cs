2ca375cebd60360317b2550896b464f10e3a5138
Mohamed Galal <Mohamed Galal@DESKTOP-28D5B9Q>
2024-07-18 20:50:27 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArithmeticCalculator
+{
+    public class Calculator<T> where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+    {
+        public delegate T Operation(T x, T y);
+
+        public Operation Add;
+        public Operation Subtract;
+        public Operation Multiply;
+        public Operation Divide;
+
+        public Calculator()
+        {
+            Add = (x, y) => (dynamic)x + (dynamic)y;
+            Subtract = (x, y) => (dynamic)x - (dynamic)y;
+            Multiply = (x, y) => (dynamic)x * (dynamic)y;
+            Divide = (x, y) => (dynamic)x / (dynamic)y;
+        }
+
+        public T AddNumber(T x, T y)
+        {
+            return Add(x, y);
+        }
+
+        public T SubstrctNumber(T x, T y)
+        {
+            return Subtract(x, y);
+        }
+
+        public T MultiplicatNumber(T x, T y)
+        {
+            return Multiply(x, y);
+        }
+
+        public T DivisionNumber(T x, T y)
+        {
+            return Divide(x, y);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericArithmeticCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var intgerNumber = new Calculator<int>();
+            Console.WriteLine("Int Calculations:");
+            Console.WriteLine($"ADD: {intgerNumber.AddNumber(43, 34)}");
+            Console.WriteLine($"Subtract: {intgerNumber.SubstrctNumber(32, 76)}");
+            Console.WriteLine($"Multiply: {intgerNumber.MultiplicatNumber(23, 7)}");
+            Console.WriteLine($"Divide: {intgerNumber.DivisionNumber(34, 56)}");
+
+            var doubleNumber = new Calculator<double>();
+            Console.WriteLine("\nDouble Calculations:");
+            Console.WriteLine($"Add: {doubleNumber.AddNumber(54.3, 4.8)}");
+            Console.WriteLine($"Subtract: {doubleNumber.SubstrctNumber(14.5, 9.6)}");
+            Console.WriteLine($"Multiply: {doubleNumber.MultiplicatNumber(45.12, 7.3)}");
+            Console.WriteLine($"Divide: {doubleNumber.DivisionNumber(65.25, 5.2)}");
+        }
+    }
+}
